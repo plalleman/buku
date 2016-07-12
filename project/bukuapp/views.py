eec5774be1b9e0544e8dc69e6dbec6759a1b75b1
plalleman <paige.alleman@colorado.edu>
2016-07-12 17:41:58 -0600

    Made hello world splash page

--- project/bukuapp/views.py
+++ project/bukuapp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, world! This is the start of buku!")
+	context = RequestContext(request)
+	context_dict = {}
+	return render_to_response('bukuapp/index.html', context_dict, context)
